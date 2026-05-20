fbd65e4433120e5d211cff96a4df099ad13798ce
Jyle <drene7450@gmail.com>
2026-05-20 13:48:55 +0800

    Remove error handling

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,9 @@
 import org.jline.terminal.Attributes;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import static org.jline.terminal.Attributes.InputFlag;
 
-void main() {
-	Terminal terminal = null;
-	try {
-		terminal = TerminalBuilder.terminal();
-	} catch (IOException e) {
-		System.out.println("Cannot proceed: IOException, " + Arrays.toString(e.getStackTrace()));
-		System.exit(1);
-	}
+void main() throws IOException {
+	var terminal = TerminalBuilder.terminal();
+
 }
