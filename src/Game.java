dd7c11dab8041b771b1d3442732e5a5ff92dd2a1
Jyle <drene7450@gmail.com>
2026-05-20 14:31:12 +0800

    Fix errors and add terminal input handling

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,8 @@
-import org.jline.terminal.Attributes;
-import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
-import static org.jline.terminal.Attributes.InputFlag;
 
 void main() throws IOException {
 	var terminal = TerminalBuilder.terminal();
-
+	terminal.enterRawMode();
+	var stream = terminal.input();
+	stream.read();
 }
