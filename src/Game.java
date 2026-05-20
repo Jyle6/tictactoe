def9e1ab392746fcd6d65b453e1e8f41ae06bac7
Jyle <drene7450@gmail.com>
2026-05-20 15:06:32 +0800

    Add opponent selector

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,21 @@
 import org.jline.terminal.TerminalBuilder;
+import static org.jline.utils.InfoCmp.Capability;
 
 void main() throws IOException {
 	var terminal = TerminalBuilder.terminal();
 	terminal.enterRawMode();
-	var stream = terminal.input();
-	stream.read();
+	var input = terminal.input();
+	var output = terminal.writer();
+	var start = terminal.getHeight();
+	output.print("Play against:\n>Human\n CPU");
+	output.flush();
+	boolean selected_human = true;
+	while (input.read() != '\r') {
+		selected_human = !selected_human;
+		terminal.puts(Capability.cursor_address, start - 3, 0);
+		output.println("Play against: ");
+		terminal.writer().print(selected_human ? ">Human\n CPU" : " Human\n>CPU");
+		output.flush();
+	}
+	terminal.close();
 }
