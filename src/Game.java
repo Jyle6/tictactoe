533da0f4a9b0ad687ed6ef15c2e44632649f6b3a
Jyle <drene7450@gmail.com>
2026-05-31 17:17:19 +0800

    Seperation of concerns

--- src/Game.java
+++ src/Game.java
@@ -5,11 +5,11 @@ import static org.jline.utils.InfoCmp.Capability;
 Terminal terminal;
 InputStream input;
 PrintWriter output;
-int start;
 boolean first = false;
 // 2D arrays are expensive in terms of memory and performance and cannot be easily checked by CPU
 char[] board = "123456789".toCharArray();
 char player = 'O';
+boolean playHuman;
 
 boolean ask_yn(String prompt) throws IOException {
 	output.print(prompt + " [y/n]");
@@ -26,36 +26,55 @@ boolean ask_yn(String prompt) throws IOException {
 }
 
 void main() throws IOException {
+	init();
+	configureGame();
+	output.print('\n');
+	while (true) {
+		printBoardState();
+		if (first && player == 'O') cpu();
+		else human();
+		player = player == 'O' ? 'X' : 'O';
+		terminal.puts(Capability.clear_screen);
+	}
+	//terminal.close();
+}
+
+private void human() throws IOException {
+	while (true) {
+		var in = input.read() - '1';
+		if (in < 0 || in > 9 || board[in] == 'X' || board[in] == 'O') {
+			output.print("\nillegal");
+			output.flush();
+			continue;
+		}
+		board[in] = player;
+		break;
+	}
+}
+
+void cpu() throws IOException {
+}
+
+void printBoardState() {
+	output.println(player + "'s turn!");
+	output.write(board, 0, 3);
+	output.print('\n');
+	output.write(board, 3, 3);
+	output.print('\n');
+	output.write(board, 6, 3);
+	output.flush();
+}
+
+void configureGame() throws IOException {
+	playHuman = ask_yn("Against a human?");
+	if (!playHuman) {
+		first = ask_yn("\nIs CPU first?");
+	}
+}
+
+void init() throws IOException {
 	terminal = TerminalBuilder.terminal();
 	terminal.enterRawMode();
 	input = terminal.input();
 	output = terminal.writer();
-	start = terminal.getHeight();
-	boolean selected_human = ask_yn("Against a human?");
-	if (!selected_human) {
-		first = ask_yn("\nIs CPU first?");
-	}
-	output.println("Initializing Tic-Tac-Toe board...");
-	start = terminal.getHeight();
-	while (true) {
-		output.println(player + "'s turn!");
-		output.write(board, 0, 3);
-		output.print('\n');
-		output.write(board, 3, 3);
-		output.print('\n');
-		output.write(board, 6, 3);
-		output.flush();
-		if (first && player == 'O') {
-			// CPU
-		} else if (player == 'X' || selected_human) {
-			var in = input.read() - '1';
-			if ((in >= 1 && in <= 9) || board[in] == 'X' || board[in] == 'O') {
-				output.print("\nillegal move\n");
-				continue;
-			}
-			board[in] = player;
-		}
-		player = player == 'O' ? 'X' : 'O';
-	}
-	//terminal.close();
 }
